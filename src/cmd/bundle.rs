8db3dec0136560e13c260653493735cfad4c8bd9
Karlo <88337245+Rqnsom@users.noreply.github.com>
2023-10-27 11:33:24 +0200

    feat: add bundle command
    Examples:
    Running the command without any options:
    ```smove bundle```
    will create a new .mvb file `build/<package_name>/bundles/<package_name>.mvb`
    
    Running the command with the name option:
    ```smove bundle -n my_bundle```
    will create a new .mvb file `build/<package_name>/bundles/my_bundle.mvb`
    
    Running the command with the modules_exclude option:
    ```smove bundle --modules_exclude Vector```
    will create a new .mvb file which won't include the Vector.mv module.

---
+++ src/cmd/bundle.rs
@@ -0,0 +1,131 @@
+use anyhow::Result;
+use clap::Parser;
+use std::collections::BTreeMap;
+use std::ffi::OsString;
+use std::fs;
+use std::path::PathBuf;
+
+use move_binary_format::access::ModuleAccess;
+use move_binary_format::CompiledModule;
+use move_command_line_common::files::MOVE_COMPILED_EXTENSION;
+use move_core_types::language_storage::ModuleId;
+use move_vm_backend_common::types::ModuleBundle;
+
+use crate::run_context::RunContext;
+use crate::run_move_cli;
+
+/// Package command bundles modules into packages.
+#[derive(Parser, Debug)]
+#[clap(about = "smove bundle")]
+pub struct Bundle {
+    #[clap(
+        short,
+        long,
+        help = "Bundle name. By default it is the same as the name of the pacakge."
+    )]
+    name: Option<String>,
+
+    // Modules are taken from the <PROJECT_PATH>/build/<PROJECT_NAME>/bytecode_modules directory.
+    // The names are case-insensitive and can be specified with an extension.mv or without it.
+    #[clap(
+        help = "Names of modules to exclude from the bundling process.",
+        long = "modules_exclude",
+        multiple_values = true
+    )]
+    modules_exclude: Vec<String>,
+}
+
+impl Bundle {
+    /// Executes the command.
+    pub fn run(&mut self, ctx: &RunContext) -> Result<()> {
+        // Build all move modules
+        run_move_cli::execute_build(ctx)?;
+
+        // Add extension to filtered modules to make comparsion more easy.
+        let mut modules_exclude_ext = Vec::with_capacity(self.modules_exclude.len());
+        while let Some(module) = self.modules_exclude.pop() {
+            modules_exclude_ext.push(OsString::from(module + "." + MOVE_COMPILED_EXTENSION))
+        }
+
+        // Get all bytecode modules (without external dependecies and without excluded ones)
+        let module_paths = ctx
+            .get_bytecode_modules()?
+            .into_iter()
+            .filter(|module| {
+                !modules_exclude_ext.contains(&module.file_name().unwrap().to_os_string())
+            })
+            .collect::<Vec<PathBuf>>();
+
+        let modules: Vec<Vec<u8>> = module_paths.into_iter().flat_map(std::fs::read).collect();
+
+        let sorted_modules = sort_modules(modules);
+        let bundle = ModuleBundle::new(sorted_modules);
+
+        // Path to the output file
+        let bundle_name = match self.name {
+            Some(ref name) => name,
+            _ => ctx.manifest.package.name.as_str(),
+        };
+        let output_file_path = ctx.bundle_output_path(bundle_name)?;
+        if output_file_path.exists() {
+            fs::remove_file(&output_file_path)?;
+        }
+
+        fs::write(&output_file_path, bundle.encode()?)?;
+
+        println!(
+            "Modules are bundled under: {}",
+            output_file_path
+                .canonicalize()
+                .unwrap_or_default()
+                .display()
+        );
+
+        Ok(())
+    }
+}
+
+/// Returns module bytecode in sorted order according to dependecies.
+fn sort_modules(modules: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    let mut map = modules
+        .into_iter()
+        .map(|bytecode| {
+            let module = CompiledModule::deserialize(&bytecode).unwrap();
+            (module.self_id(), (bytecode, module))
+        })
+        .collect::<BTreeMap<_, _>>();
+
+    let mut order = vec![];
+    for id in map.keys() {
+        sort_by_deps(&map, &mut order, id.clone());
+    }
+
+    let mut result = vec![];
+    for id in order {
+        let (bytecode, _) = map.remove(&id).unwrap();
+        result.push(bytecode)
+    }
+
+    result
+}
+
+/// Recursively sorts dependencies for compiled Move modules.
+fn sort_by_deps(
+    map: &BTreeMap<ModuleId, (Vec<u8>, CompiledModule)>,
+    order: &mut Vec<ModuleId>,
+    id: ModuleId,
+) {
+    if order.contains(&id) {
+        return;
+    }
+
+    let (_, compiled_module) = &map.get(&id).unwrap();
+    for dep in compiled_module.immediate_dependencies() {
+        // Only consider deps which are actually in this package. Deps for outside
+        // packages are considered fine because of package deployment order.
+        if map.contains_key(&dep) {
+            sort_by_deps(map, order, dep);
+        }
+    }
+    order.push(id)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,46 +8,46 @@ mod cmd;
 mod run_context;
 mod run_move_cli;
 
-/// CLI frontend for the Move compiler and VM in Substrate
+/// CLI frontend for the Move compiler and VM in Substrate.
 #[derive(Parser)]
 #[clap(name = "smove", author, about, long_about = None)]
 struct SmoveArgs {
-    /// Native move-cli arguments
+    /// Native move-cli arguments.
     #[clap(flatten)]
     move_args: Move,
 
-    /// Commands
+    /// Commands.
     #[clap(subcommand)]
     cmd: SmoveCommand,
 }
 
-/// Move CLI and smove subcommands
+/// Move CLI and smove subcommands.
 #[derive(clap::Subcommand)]
 enum SmoveCommand {
-    /// Native move-cli commands
+    /// Native move-cli commands.
     #[clap(flatten)]
     MoveCommand(move_cli::Command),
 
-    /// Create package bundle
+    /// Create package bundle.
     #[clap(about = "Create package bundle")]
-    Package {
+    Bundle {
         #[clap(flatten)]
-        cmd: cmd::package::Package,
+        cmd: cmd::bundle::Bundle,
     },
 
-    /// Run the script and create a transcation for the pallet
+    /// Run the script and create a transcation for the pallet.
     #[clap(about = "Run the script and create a transcation for the pallet")]
     Run {},
 }
 
-/// Run the smove CLI
+/// Run the smove CLI.
 pub fn smove_cli(cwd: PathBuf) -> Result<()> {
     let SmoveArgs { move_args, cmd } = SmoveArgs::parse();
     let ctx = RunContext::new(cwd, move_args)?;
 
     match cmd {
         SmoveCommand::MoveCommand(cmd) => run_move_cli::run_command(&ctx, cmd),
-        SmoveCommand::Package { mut cmd } => cmd.run(&ctx),
+        SmoveCommand::Bundle { mut cmd } => cmd.run(&ctx),
         SmoveCommand::Run {} => unimplemented!(),
     }
 }
