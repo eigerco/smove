ad2e105053d2b12c94fa3b55e3a8e80e5688837c
Karlo <88337245+Rqnsom@users.noreply.github.com>
2023-12-20 17:33:45 +0100

    feat: RPC command for estimate gas for publishing a bundle

--- src/cmd/node/rpc/estimate_gas_publish.rs
+++ src/cmd/node/rpc/estimate_gas_publish.rs
@@ -18,31 +18,6 @@ pub struct EstimateGasPublishModule {
     module_path: PathBuf,
 }
 
-/// Gas estimation information.
-#[derive(Deserialize)]
-struct Estimation {
-    /// Gas used.
-    gas_used: u64,
-    /// Status code for the MoveVM execution.
-    vm_status_code: StatusCode,
-}
-
-impl fmt::Display for Estimation {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let gas_used = if matches!(self.vm_status_code, StatusCode::EXECUTED) {
-            self.gas_used
-        } else {
-            0
-        };
-
-        write!(
-            f,
-            "Estimate (gas_used: {}, vm_status_code: {:?})",
-            gas_used, self.vm_status_code
-        )
-    }
-}
-
 impl EstimateGasPublishModule {
     /// Executes the command.
     pub fn execute(&mut self) -> Result<()> {
@@ -65,6 +40,66 @@ impl EstimateGasPublishModule {
         println!("Estimated gas: {estimated_gas}");
 
         Ok(())
+    }
+}
+
+/// Estimate gas for publishing a bunlde.
+#[derive(Parser, Debug)]
+#[clap(about = "Estimate gas for publishing a bundle")]
+pub struct EstimateGasPublishBundle {
+    #[clap(short, long, help = "Account ID in the SS58 format")]
+    account_id: String,
+    #[clap(short, long, help = "Path to the bundle (compiled by the smove)")]
+    bundle_path: PathBuf,
+}
+
+impl EstimateGasPublishBundle {
+    /// Executes the command.
+    pub fn execute(&mut self) -> Result<()> {
+        // TODO: provide the rpc_url via the argument
+        let rpc_url = "http://localhost:9944/";
+
+        let script_tx = read_bytes(&self.bundle_path)?;
+
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()?;
+
+        let client = HttpClientBuilder::default().build(rpc_url)?;
+        let params = rpc_params![&self.account_id, script_tx];
+        let response: Result<Estimation, _> =
+            rt.block_on(async { client.request("mvm_estimateGasPublishBundle", params).await });
+
+        let estimated_gas = response.with_context(|| "RPC result failure")?;
+
+        println!("Estimated gas: {estimated_gas}");
+
+        Ok(())
+    }
+}
+
+/// Gas estimation information.
+#[derive(Deserialize)]
+struct Estimation {
+    /// Gas used.
+    gas_used: u64,
+    /// Status code for the MoveVM execution.
+    vm_status_code: StatusCode,
+}
+
+impl fmt::Display for Estimation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let gas_used = if matches!(self.vm_status_code, StatusCode::EXECUTED) {
+            self.gas_used
+        } else {
+            0
+        };
+
+        write!(
+            f,
+            "Estimate (gas_used: {}, vm_status_code: {:?})",
+            gas_used, self.vm_status_code
+        )
     }
 }
 
