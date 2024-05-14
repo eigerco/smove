dd97183957e5d84b27b57c49ee0c0b2600ac5bf4
Karlo <88337245+Rqnsom@users.noreply.github.com>
2024-05-14 13:33:15 +0200

    chore: remove obsolete gas-to-weight rpc

--- src/cmd/node/rpc/mod.rs
+++ src/cmd/node/rpc/mod.rs
@@ -2,12 +2,28 @@
 
 pub(super) mod estimate_gas_execute;
 pub(super) mod estimate_gas_publish;
-pub(super) mod gas_to_weight;
 pub(super) mod get_module_abi;
 
 use move_core_types::vm_status::StatusCode;
 use serde::Deserialize;
 use std::fmt;
+use std::path::Path;
+
+/// Reads bytes from a file for the given path.
+fn read_bytes(file_path: &Path) -> Result<Vec<u8>> {
+    std::fs::read(file_path)
+        .map_err(anyhow::Error::from)
+        .with_context(|| format!("Failure to read filename {}", file_path.display()))
+}
+
+#[derive(Debug, Default, Copy, Clone, Deserialize)]
+#[allow(unused)]
+struct Weight {
+    /// The weight of computational time used based on some reference hardware.
+    ref_time: u64,
+    /// The weight of storage space used by proof of validity.
+    proof_size: u64,
+}
 
 /// Gas estimation information.
 #[derive(Deserialize)]
@@ -16,19 +32,22 @@ struct Estimation {
     gas_used: u64,
     /// Status code for the MoveVM execution.
     vm_status_code: StatusCode,
+    /// Substrate weight required for the complete extrinsic cost combined with the variable
+    /// gas indicated in the `Estimation` struct.
+    total_weight_including_gas_used: Weight,
 }
 
 impl fmt::Display for Estimation {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let gas_used = if matches!(self.vm_status_code, StatusCode::EXECUTED) {
-            self.gas_used
+        let (gas_used, weight) = if matches!(self.vm_status_code, StatusCode::EXECUTED) {
+            (self.gas_used, self.total_weight_including_gas_used)
         } else {
-            0
+            (0, Weight::default())
         };
 
         write!(
             f,
-            "Estimate (gas_used: {gas_used}, vm_status_code: {:?})",
+            "Estimate (gas_used: {gas_used}, total_extrinsic cost with gas_used: {weight:?}, vm_status_code: {:?})",
             self.vm_status_code
         )
     }
